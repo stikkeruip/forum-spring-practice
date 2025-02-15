9472f834b66ce8cb3cd4d3d08ade5515602b2c84
uipko <stikkeruipko@gmail.com>
2025-02-15 07:39:28 +0200

    Initializes Spring Boot + Maven

---
+++ forum-backend/src/main/java/com/uipko/forumbackend/ForumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uipko.forumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumBackendApplication.class, args);
+	}
+
+}
---
+++ forum-backend/src/test/java/com/uipko/forumbackend/ForumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uipko.forumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
